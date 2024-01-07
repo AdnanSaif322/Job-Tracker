2a6e17d57b917c2a39b4a9f49a235ec9b3023d53
Adnan <adnan.saif.322@gmail.com>
2024-01-07 21:21:03 +0600

    landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,4 +1,36 @@
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
+import Logo from "../components/index";
+
 const Landing = () => {
-  return <h1>Landing Page</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque
+            enim eius voluptatem, rerum quasi rem quam aspernatur porro ipsa
+            distinctio, unde eveniet fugit repudiandae veritatis corrupti
+            molestias in, placeat expedita.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login/Demo User
+          </Link>
+        </div>
+        <img src={main} className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
+
 export default Landing;
