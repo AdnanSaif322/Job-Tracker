02fa1704484bc41b28f408ab43a0bbdf21ae4939
Adnan <adnan.saif.322@gmail.com>
2024-01-07 23:25:16 +0600

    register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,38 @@
 import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { Logo, FormRow } from "../components";
+
 const Register = () => {
   return (
-    <>
-      <h1>Register Page</h1>
-      <Link to="/login">Login</Link>
-    </>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow
+          type="text"
+          name="firstname"
+          labelText="first name"
+          defaultValue="adnan"
+        />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="last name"
+          defaultValue="saif"
+        />
+        <FormRow type="email" name="email" defaultValue="saif@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="Saif@123456" />
+        <button type="submit" className="btn btn-block">
+          Register
+        </button>
+        <p>
+          Already a memeber?
+          <Link to="/login" className="member-btn">
+            Log in
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 export default Register;
